93f183f4b0869e3cc7c970c0f58b47190ace9b1e
이채연 <196144872+codus69@users.noreply.github.com>
2026-07-20 11:37:50 +0900

    feat: Fetch topics dynamically for navigation and update read page to display topic details

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,10 +1,12 @@
 export default async function Read(props) {
   const { id } = await props.params;
   console.log('Read Page 작동');
+  const response = await fetch(`http://localhost:9999/topics/${id}`);
+  const topics = await response.json();
   return (
     <>
-      <h2>Read</h2>
-      <p>parameter:{id}</p>
+      <h2>{topics.title}</h2>
+      <p>{topics.message}</p>
     </>
   );
 }
