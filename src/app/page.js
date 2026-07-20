270c276f1ab8dcbe63061ed29e30dd031318e3f2
이채연 <196144872+codus69@users.noreply.github.com>
2026-07-20 15:45:29 +0900

    feat: Controls.js 추가 / update 수정

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <>
       <h1>Next.js</h1>
       <p>Hello, world</p>
-      {/* <Image src="/home.png" width={50} height={50} alt="Picture of the author" /> */}
+      <Image src="/home.png" width={50} height={50} alt="Picture of the author" />
     </>
   );
 }
--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,12 +1,36 @@
-export default async function Read(props) {
-  const { id } = await props.params;
-  console.log('Read Page 작동');
-  const response = await fetch(`http://localhost:9999/topics/${id}`);
-  const topics = await response.json();
+'use client';
+import { useRouter } from 'next/navigation';
+
+export default function Update() {
+  console.log('Update Page 작동');
+  const router = useRouter();
+
   return (
     <>
-      <h2>{topics.title}</h2>
-      <p>{topics.message}</p>
+      <h3 style={styles.title}>Update Form</h3>
+      <form action="">
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">
+            title
+          </label>
+          <input type="text" className="form-control" id="title" name="title" placeholder="글 제목을 입력해주세요" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="message" className="form-label">
+            Message
+          </label>
+          <textarea name="message" className="form-control" id="message" rows="3"></textarea>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          입력
+        </button>
+      </form>
     </>
   );
 }
+
+const styles = {
+  title: {
+    color: 'green',
+  },
+};
